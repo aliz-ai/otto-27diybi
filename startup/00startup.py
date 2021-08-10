1a6378da91134208a63516aea29cfa1cd40bfc12
Moricz Tamas <tamas.moricz@aliz.ai>
2021-08-10 16:22:54 +0200

    refactor fix

--- startup/00startup.py
+++ startup/00startup.py
@@ -8,7 +8,7 @@ person_name = output[1]
 
 # Updating the BigQuery magic default query job config, so that BQ cell magics automatically have a team_name and person_name label applied to them.
 if not magics.context.default_query_job_config.labels:
-    magics.context.default_query_job_config.labels = {'team_name': teamname, 'person_name': personname}
+    magics.context.default_query_job_config.labels = {'team_name': team_name, 'person_name': person_name}
 else:
-    magics.context.default_query_job_config.labels['team_name'] = teamname
-    magics.context.default_query_job_config.labels['person_name'] = personname
+    magics.context.default_query_job_config.labels['team_name'] = team_name
+    magics.context.default_query_job_config.labels['person_name'] = person_name
