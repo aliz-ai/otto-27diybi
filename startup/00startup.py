bd1dd1b17fa958be8eed4f0127dceda84764963f
MoriczTamasAliz <58034246+MoriczTamasAliz@users.noreply.github.com>
2021-09-06 09:38:18 +0200

    updated notebook name schema

--- startup/00startup.py
+++ startup/00startup.py
@@ -4,7 +4,7 @@ from google.cloud.bigquery import magics
 # We are assuming all notebooks have a name of teamname-personname-notebookname
 output = subprocess.check_output("hostname", shell=True).decode('utf-8').split('-')
 team_name = output[0]
-person_name = output[1]
+person_name = output[1:-1]
 
 # Updating the BigQuery magic default query job config, so that BQ cell magics automatically have a team_name and person_name label applied to them.
 if not magics.context.default_query_job_config.labels:
