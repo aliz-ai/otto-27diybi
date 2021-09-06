4f24401db793232abf129d83b49516cf27191ec6
MoriczTamasAliz <58034246+MoriczTamasAliz@users.noreply.github.com>
2021-09-06 09:58:31 +0200

    Changed to fit bucket name schema

--- notebook_backup/backup.py
+++ notebook_backup/backup.py
@@ -13,8 +13,8 @@ def backup_project_notebooks(request):
 
     def bucket_fits_schema(bucket):
         # We are assuming buckets are either in the format of teamname-personname-personal or teamname-common-team
-        if len(bucket.split('-')) == 3:
-            return bucket.split('-')[-1] in ['personal', 'team']
+        if len(bucket.split('-')) >= 3:
+            return bucket.split('-')[-1] in ['collaboration', 'backup']
         
     def blob_allowed_to_stay(blob):
         return blob.split('.')[-1] in WHITELISTED_EXTENSIONS
@@ -26,12 +26,12 @@ def backup_project_notebooks(request):
     for bucket in all_relevant_buckets:
         bucket_name_array = bucket.name.split('-')
         team_name = bucket_name_array[0]
-        personal = (bucket_name_array[-1] == 'personal')
+        personal = (bucket_name_array[-1] == 'backup')
 
         if personal:
             all_blobs = gcs_client.list_blobs(bucket)
             all_relevant_blobs = [blob for blob in all_blobs if blob_allowed_to_stay(blob.name)]
-            destination_bucket = gcs_client.bucket(f"{team_name}-common-team")
+            destination_bucket = gcs_client.bucket(f"{team_name}-ml-collaboration")
 
             for blob in all_relevant_blobs:
-                bucket.copy_blob(blob, destination_bucket, blob.name)
+                bucket.copy_blob(blob, destination_bucket, f"{bucket.name}/{blob.name}")
